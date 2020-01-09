2815f4ac09719fcf89f4a5a59954f228a7c49c7c
Jack Walters <jackwalters@Jacks-MacBook-Pro.local>
2020-01-08 21:12:02 -0500

    incorporated loop until 'quit' is typed

--- convert.py
+++ convert.py
@@ -2,18 +2,26 @@
 #Script meant to convert letters to numbers and opposite for my Cryptography class
  
 def convert_to_num(letter) :
-	return ord(letter) - 97
+	if (len(letter) > 1):
+		return('Enter valid value')
+	return ord(letter) - 97 #ord for ascii value of letter
 
 def convert_to_letter(num) :
 	num = int(num) % 26
-	return chr(num + 97)
+	return chr(num + 97) #chr for letter from ascii value
 
-val = input("Enter value to be converted: ")
+val = input("Enter value to be converted ('quit' to quit) : ")
 
-if (val.isalpha()) :
-	print(convert_to_num(val.lower()))
-else :
-	print(convert_to_letter(val))
+while (val != 'quit') :
+
+	if (val.isalpha()) :
+		print(convert_to_num(val.lower()))
+	elif(val.isnumeric()) :
+		print(convert_to_letter(val))
+	else :
+		print('Enter valid value')
+
+	val = input("Enter value to be converted ('quit' to quit) : ")
 
 
 
